b28cd22729f600b154a46be4b3058f5ef89f1e1c
Aabhaas <agupt223@asu.edu>
2020-04-09 02:12:49 -0700

    Completed Search Autocomplete

--- myscript.js
+++ myscript.js
@@ -1,5 +1,5 @@
 firstTimeFocus = true
-
+conditions = []
 function firstTimeSearchFocus() {
     if(firstTimeFocus)
     {
@@ -10,6 +10,7 @@ function firstTimeSearchFocus() {
     }
     $(".searchbox").css({"animation-name":"searchMoveUp", "animation-duration":"1s"}).css({"top":"10%"});
 }
+
 function csvJSON(csv){
     var lines=csv.split("\n");
     var result = [];
@@ -26,17 +27,14 @@ function csvJSON(csv){
             //     obj[headers[j]] = lines[i-1].split(",")[j];
             // }
             obj[headers[j]] = currentline[j];
-            if(headers[j]=="Condition")
-            {
-                console.log(i, currentline[j]);
-            }
         }
   
         result.push(obj);
   
     }
     return result; //JSON
-  }
+}
+
 function getColAsArray(Json, key){
     
     arr = [];
@@ -47,6 +45,7 @@ function getColAsArray(Json, key){
     }
     return arr;
 }
+
 $(document).ready(function(){
     $.ajax({
         url:"data/webMD_part3.csv",
@@ -54,11 +53,45 @@ $(document).ready(function(){
         success:function(data)
         {
             myJ = csvJSON(data)
-            arr = getColAsArray(myJ,"Condition");
-            // for (i=0;i<arr.length;i++)
+            conditions = getColAsArray(myJ,"Condition");
+            conditions.pop()
+            conditions = Array.from(new Set(conditions))
+
+            // for (i=0;i<conditions.length-1;i++)
             // {
-            //     console.log(arr[i]);
+            //     console.log(i,conditions[i]);
             // }
         }
     })
+    $( ".input" ).keyup(function(data) {
+        
+        $('.searchOpt').off('click');
+        let searchOptions = []
+        
+        searchOptions = conditions.filter(condition => condition.toLowerCase().includes(data.target.value.toLowerCase()));
+        if(data.target.value == '')
+        {
+            searchOptions =[]
+        }
+
+        searchOptions = searchOptions.map(searchOption => `<li class=searchOpt value=${searchOption}>${searchOption}</li>`)
+        $(".searchList").html(!searchOptions ? '' : searchOptions.join(''));
+        // console.log(searchOptions);
+        
+        $('.searchOpt').on('click', function(){
+            console.log("hii")
+            $(".input").val($(this).text())
+            $('.searchOpt').off('click');
+            $(".searchList").html('');
+            $(".input").on('focusout', function(){
+                $(".searchList").html('');
+                $(".input").off('focusout')
+            })
+        });
+        
+    });
+    
 });
+
+
+
